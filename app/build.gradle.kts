d0e266395e761da3c693103666c335884620d440
Brie <briecrafty@gmail.com>
2025-01-31 01:18:37 -0600

    Adding Unit tests to my code. One checks for search functionality and other tests the save button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.espresso.contrib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/androidTest/java/com/example/myvacation/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myvacation/ExampleInstrumentedTest.java
@@ -3,23 +3,32 @@ package com.example.myvacation;
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.content.Context;
+import android.icu.text.SimpleDateFormat;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,7 +37,10 @@ import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
 import com.example.myvacation.UI.MainActivity;
+import com.example.myvacation.UI.VacationDetails;
 import com.example.myvacation.UI.VacationList;
+
+import java.util.Locale;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -48,9 +60,6 @@ public class ExampleInstrumentedTest {
             new ActivityScenarioRule<>(VacationList.class);
     @Test
     public void testSearchViewInputFiltersRecyclerView() throws InterruptedException {
-
-
-
         // Type a query into the SearchView
         onView(withId(R.id.searchView)).check(matches(isDisplayed()));
         onView(withId(R.id.searchView)).perform(click());
@@ -62,6 +71,34 @@ public class ExampleInstrumentedTest {
         // Assert RecyclerView displays filtered results
         onView(withId(R.id.vacationlistrecyclerview))
                 .check(matches(hasDescendant(withText("example"))));
+    }
+
+
+    @Rule
+    public ActivityScenarioRule<VacationDetails> vacationDetailsRule =
+            new ActivityScenarioRule<>(VacationDetails.class);
+    @Test
+    public void testVacationDetailsSave() throws InterruptedException {
+        //Testing Vacation Title
+        int vacationTitle=R.id.VacationTitle;
+        int vacationHotel=R.id.VacationHotelName;
+
+
+        onView(withId(vacationTitle)).check(matches(isDisplayed()));
+        onView(withId(vacationTitle)).perform(click());
+        onView(withId(vacationTitle)) //
+                .perform(typeText("exampleVacation"));
+
+        //Testing Hotel Name
+        onView(withId(vacationHotel)).check(matches(isDisplayed()));
+        onView(withId(vacationHotel)).perform(click());
+        onView(withId(vacationHotel)) //
+                .perform(typeText("exampleHotel"));
+
+//Save Vacation - Empty Dates
+        onView(withContentDescription("More options")).perform(click());
+        onView(withText("Save Vacation")).perform(click());
+
     }
 
 }
